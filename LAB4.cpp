5d7930f7bc14284801d5d03233ce767ab22439fd
chhavi <122530834+ChhaviSingh02@users.noreply.github.com>
2025-06-29 23:26:30 +0530

    Add files via upload

---
+++ LAB4.cpp
@@ -0,0 +1,10 @@
+// Date: 6/2/2025
+// Cloud Computing Lab Exercise 4
+// Socket Programming in C++ on a Virtual Machine
+
+// Question: Developing a Simple Client-Server Model using TCP Sockets in C++ on a Virtual Machine.
+// Lab Task Description:
+// You are required to set up a virtual machine (VM) with a Linux operating system and install necessary tools like GCC for C++ programming. Inside this VM, you will develop a TCP-based client-server model, where:
+// * The server will listen for incoming client connections, accept a connection, receive a message, and send a response.
+// * The client will connect to the server, send a message, and receive the server’s response.
+// You will then compile, execute, and test the client-server model on the virtual machine.
